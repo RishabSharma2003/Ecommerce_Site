89edc3bf378e9ef978bd1d7f3949908423eeb040
RishabSharma2003 <141629276+RishabSharma2003@users.noreply.github.com>
2024-09-03 23:53:17 +0530

    commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+    <header className="App-header" onClick={()=>{
+      console.log("hi")
+    }}>
+      <h1 className="translate-y-0.5 drop-shadow-xl backdrop-brightness-200 hover:drop-shadow-xl drop-shadow-sm blur-md invert drop-shadow-xl md:filter-none text-4xl font-black font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </header>
+  </div>
+  )
+}
+
+export default App;
