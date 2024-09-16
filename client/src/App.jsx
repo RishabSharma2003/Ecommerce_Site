3939b41f575bc5459952e1161268ba55252f702a
ombansal1234 <abc426037@gmail.com>
2024-09-16 12:33:59 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Navigation from './pages/auth/Navigation';
+import { ToastContainer } from "react-toastify";
 
 const Main = () => {
   return (
     <div>
+      <ToastContainer/>
       <Navigation/>
       <main className='py-3'>
         <Outlet/>
